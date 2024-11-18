67cbd0c34d28cf282ba23eb339b68865c3857dd2
Scr1p4ch <miroshnichenkoucheba@gmail.com>
2024-11-18 15:52:47 +0300

    modified:   Utility/Glossary.h 	modified:   Utility/ProductClass.h

--- Utility/Glossary.h
+++ Utility/Glossary.h
@@ -1,6 +1,14 @@
 #ifndef _GLOSSARY_H_
 #define _GLOSSARY_H_
 
+#include <string>
+
+std::string name_1[10] = {"Interesting ", "Good ", "Enjoyable ", "Nice ", "Absorbing ", "Adventurous ", "Gripping ", "Fascinating ", "Exciting ", "Ete-catching "};
+std::string name_2[10] = {"knife", "scissors", "blender", "mixer", "boot", "cleaner", "TV", "backpack", "bag", "holder"};
+std::string country[] = {"China", "Russia", "India", "Indonesia", "Brazil"};
+std::string host[] = {"Artist", "Kitchener", "Airman", "Countryman", "Clerk", "Footballer"};
+
+
 
 
 #endif
--- Utility/ProductClass.h
+++ Utility/ProductClass.h
@@ -8,7 +8,7 @@
 
 class Product {
     std::string name;
-    std::string ownerCompany;
+    std::string country;
     std::string showHost;
     double soldCopK;
     unsigned int duration; //in minutes;
@@ -16,18 +16,18 @@ class Product {
 public: 
     Product() = default;
 
-    Product(std::string _name, std::string _ownerCompany, std::string _showHost, 
+    Product(std::string _name, std::string _country, std::string _showHost, 
     double _soldCopK, unsigned int _duration)
-     : name(_name), ownerCompany(_ownerCompany), showHost(_showHost), soldCopK(_soldCopK), duration(_duration) {}
+     : name(_name), country(_country), showHost(_showHost), soldCopK(_soldCopK), duration(_duration) {}
 
 
-    Product(Product && other) noexcept : name(Move(other.name)), ownerCompany(Move(other.ownerCompany)),
+    Product(Product && other) noexcept : name(Move(other.name)), country(Move(other.country)),
         showHost(Move(other.showHost)), soldCopK(other.soldCopK), duration(other.duration) {}
 
     Product & operator=(Product && other) noexcept {
         if (this != &other) {
             name = Move(other.name);
-            ownerCompany = Move(other.ownerCompany);
+            country = Move(other.country);
             showHost = Move(other.showHost);
             soldCopK = other.soldCopK;
             duration = other.duration;
